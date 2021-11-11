bb45785a85aeb7cd9e59eb80c68ef3fac50bddc6
Yoko <votrecandidature@yahoo.fr>
2021-11-11 19:33:23 +0100

    main

--- bedrock/web/app/themes/deliciozo/index.php
+++ bedrock/web/app/themes/deliciozo/index.php
@@ -1,39 +1,40 @@
 <?php get_header(); ?>
 
 <!--insertion d'1 boucle-->
-<!-- <?php if (have_posts()) :
-        while (have_posts()) :
-          the_post();
-          the_title();
-          echo '<br> <br> <br> <br>';
-        endwhile;
-      else :
-        echo "y a pas de résultat";
-      endif;
 
-
-
-      ?> -->
 <section>
-  <div class="container">
-    <div class="card-group">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
+   <div class="container">
+      <?php if (have_posts()) : ?>
+         <?php while (have_posts()) :  the_post(); ?>
+
+            <div class="card-group">
+               <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+               </div>
+               <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+               </div>
+            </div>
+         <?php endwhile; ?>
+      <?php else : ?>
+         <div class="row">
+            <div class="col-xs-12">
+               <p>y a pas de résultats</p>
+            </div>
+            <!--fin row-->
+         <?php endif; ?>
+         </div>
+         <!--fin container-->
 
 
 </section>
