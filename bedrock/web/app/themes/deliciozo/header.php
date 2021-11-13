96fced40ea03ae7eac0f48d0b60f2f9009894b1a
Yoko <votrecandidature@yahoo.fr>
2021-11-13 16:07:08 +0100

    main

--- bedrock/web/app/themes/deliciozo/header.php
+++ bedrock/web/app/themes/deliciozo/header.php
@@ -6,7 +6,7 @@
   <title>Deliciozo Pizzeria</title>
 
 
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/logo.ico" type="image/x-icon"></a>
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/ikone_pizza.png" type="image/x-icon"></a>
   <!-- Required meta tags -->
   <!-- balise wp remplace "utf-8"-->
   <meta charset="<?php bloginfo('charset'); ?>">
@@ -22,27 +22,63 @@
 <body>
   <header>
 
-    <nav class="navbar navbar-expand-md navbar-light bg-transparent fixed-top">
+    <!-- <nav class="navbar navbar-expand-md navbar-light bg-transparent fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand mb-20px" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> -->
 
-        <div class="collapse navbar-collapse" id="main-menu">
-          <?php
-          wp_nav_menu(array(
-            'theme_location' => 'main-menu',
-            'container' => false,
-            'menu_class' => '',
-            'fallback_cb' => '__return_false',
-            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-            'depth' => 2,
-            'walker' => new bootstrap_5_wp_nav_menu_walker()
-          ));
-          ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <!--fonction qui permet de ramener à la page accueil-->
+      <div class="container-fluid">
+        <a class="navbar-brand my-auto" href="<?php echo home_url('/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/ikone_pizza.png" alt="" width="45" height="auto"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?php echo home_url('/'); ?>">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Carte</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Commande</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Galerie Photos</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nos adresses
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Paris 1er</a></li>
+                <li><a class="dropdown-item" href="#">Saint-Mandé</a></li>
+                <li><a class="dropdown-item" href="#">Neuilly/Seine</a></li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </div>
+    </nav>
+
+    <div class="collapse navbar-collapse" id="main-menu">
+      <?php
+      wp_nav_menu(array(
+        'theme_location' => 'main-menu',
+        'container' => false,
+        'menu_class' => '',
+        'fallback_cb' => '__return_false',
+        'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+        'depth' => 2,
+        'walker' => new bootstrap_5_wp_nav_menu_walker()
+      ));
+      ?>
+    </div>
+    </div>
     </nav>
     </div>
 
