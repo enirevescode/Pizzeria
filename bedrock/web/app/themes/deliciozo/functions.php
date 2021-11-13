5f5148362ea0ef0b2a34ecf1d3981fad97780ee3
Yoko <votrecandidature@yahoo.fr>
2021-11-13 11:20:45 +0100

    main

--- bedrock/web/app/themes/deliciozo/functions.php
+++ bedrock/web/app/themes/deliciozo/functions.php
@@ -1,21 +1,21 @@
 <?php
 //création d'1 constante pour la version. dans la fonction il ne faut pas //mettre les '' car c'est 1 constante !
-define('sgmon_version', '1.0.6');
+define('sgdelicio_version', '1.0.6');
 //chargement de scripts - dar 1 n. de fonction propre
 //chargement dans le front-end
 function sgmontheme_scripts() {	
 	// chargement des styles
 	//lien bootstrap qui se charge en 1er et ensuite le mien
-		wp_enqueue_style( 'sgmon_bootsrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), sgmon_version , 'all' );
+		wp_enqueue_style( 'sgdelicio_bootsrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), sgdelicio_version , 'all' );
 	//lien avec style
-		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('sgmon_bootsrap-style'), sgmon_version , 'all' );
+		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('sgdelicio_bootsrap-style'), sgdelicio_version , 'all' );
 	//lien avec js true pour qu'il aille dans le footer
 	//là où doit se situer le js
 	// chargement des scripts
 	// pour que le menu hamburger fonctionne il faut télécharger la library js de bootstrap et donner son chemin
-	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), sgmon_version, true );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), sgdelicio_version, true );
 //montheme.js est 1 fichier que g créé pour mettre des fonctions .js
-	wp_enqueue_script( 'sgmon_script', get_template_directory_uri() . '/js/montheme.js', array('jquery', 'bootstrap-js'), sgmon_version, true );
+	wp_enqueue_script( 'sgdelicio_script', get_template_directory_uri() . '/js/montheme.js', array('jquery', 'bootstrap-js'), sgdelicio_version, true );
 	
 
 }
@@ -23,16 +23,16 @@ function sgmontheme_scripts() {
 add_action( 'wp_enqueue_scripts', 'sgmontheme_scripts' );
 
 // chargement dans l'admin de wp
-function sgmon_admin_scripts() {
+function sgdelicio_admin_scripts() {
 
     //chargement des styles dans l'admin (can serve pr les créations de plugin)
-    wp_enqueue_style('sgmon_bootsrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), sgmon_version);
+    wp_enqueue_style('sgdelicio_bootsrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), sgdelicio_version);
 }
-add_action( 'admin_init', 'sgmon_admin_scripts');
+add_action( 'admin_init', 'sgdelicio_admin_scripts');
 
 ///////////////////////////////////// UTILITAIRES
 
-function sgmon_setup() {
+function sgdelicio_setup() {
 	// support vignettes, images à la une
 
 	add_theme_support('post-thumbnails');
@@ -51,14 +51,14 @@ function sgmon_setup() {
 	require_once('includes/bootstrap-5-wordpress-navbar-walker-main.php');
 }
 
-	add_action( 'after_setup_theme', 'sgmon_setup');
+	add_action( 'after_setup_theme', 'sgdelicio_setup');
 
 
 	///////////////////////////// Affichage date + Catégories utiles pour les articles
 
 	//ce modèle d'affichage 29 novembre 2016 
 
-	function sgmon_give_me_meta_01($date1, $date2, $cat, $tags) {
+	function sgdelicio_give_me_meta_01($date1, $date2, $cat, $tags) {
 		$chaine = 'publié le <time class="entry-date" datetime="';
 		$chaine .= $date1;
 		$chaine .= '">';
@@ -73,8 +73,8 @@ function sgmon_setup() {
 
 	////////////////// modifie le texte d'excerpt dans 1 article tronqué
 
-	function sgmon_excerpt_more( $more ) {
+	function sgdelicio_excerpt_more( $more ) {
 		return ' <a class="more-link" href=" '.get_permalink() .'">' . '[lire la suite]' . '</a>';
 
 	}
-	add_filter('excerpt_more', 'sgmon_excerpt_more');
+	add_filter('excerpt_more', 'sgdelicio_excerpt_more');
